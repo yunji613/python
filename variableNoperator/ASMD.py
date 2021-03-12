d815751e44802790b63482847a0e802173bd0c57
yunji613 <80086230+yunji613@users.noreply.github.com>
2021-03-12 16:08:13 +0900

    Add files via upload

---
+++ variableNoperator/ASMD.py
@@ -0,0 +1,20 @@
+# 두 정수를 입력받아 사칙연산 결과를 출력
+# #은 라인 끝까지 주석
+# '''부터 '''까지 블록 주석
+'''a = int(input("첫 번째 숫자 : "))
+b = int(input("두 번째 숫자 : "))
+print(a,"+",b,"=",a+b)
+print(a,"-",b,"=",a-b)
+print(a,"*",b,"=",a*b)
+print(a,"/",b,"=",a/b)'''
+
+
+a = int(input("첫 번째 숫자 : "))
+b = int(input("두 번째 숫자 : "))
+print(a,"+",b,"=",a+b)
+print(a,"-",b,"=",a-b)
+print(a,"*",b,"=",a*b)
+print(a,"/",b,"=",a/b)
+
+
+
---
+++ variableNoperator/strNint.py
@@ -0,0 +1,11 @@
+# 문자열 입력
+a = input("첫 번째 숫자 : ")
+b = input("두 번째 숫자 : ")
+print(a+b)
+print("Hello" + "python")
+
+# 문자열을 숫자로 변환시키는 함수 int
+a = int(input("첫 번째 숫자 : "))
+b = int(input("두 번째 숫자 : "))
+print(a+b)        
+          
